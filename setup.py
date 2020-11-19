b2d94060f013a2e8ab4f51d6bb4d995bfd39c9e6
Lewi Uberg <lewiuberg@icloud.com>
2020-11-19 17:29:27 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+from setuptools import setup, find_packages
+
+if sys.version_info < (3, 8):
+    raise Exception('Only Python 3.8+ is supported')
+
+setup(name='import_from_github_com',
+      version='0.1',
+      description="Python module finder/loader from github, like in golang",
+      author='Lewi Uberg',
+      author_email='lewiuberg@gmail.com',
+      url='https://github.com/lewiuberg/moonshine',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples',
+                                      'tests', 'release']),
+      include_package_data=True,
+      zip_safe=False)
